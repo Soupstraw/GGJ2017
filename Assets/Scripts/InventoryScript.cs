0b627d6254da701d8890be35e83e87df72367468
Joosep Jääger <joosep.jaager@gmail.com>
2017-01-21 11:56:54 +0200

    Refactored item code.

--- Assets/Scripts/InventoryScript.cs
+++ Assets/Scripts/InventoryScript.cs
@@ -1,16 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryScript : MonoBehaviour {
+
+	public Image[] inventoryBoxes;
+
+	public SkinnedMeshRenderer penRenderer;
+	public SkinnedMeshRenderer bookRenderer;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+	void OnEnable(){
+		PickupItem.OnItemPickup += AddItem;
+	}
+
+	void OnDisable(){
+		PickupItem.OnItemPickup -= AddItem;
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void AddItem(ItemScript item){
+		switch (item.itemType) {
+		case ItemType.BOOK:
+			if(bookRenderer != null)
+			bookRenderer.enabled = true;
+			break;
+		case ItemType.PEN:
+			if(penRenderer != null)
+			penRenderer.enabled = true;
+			break;
+		}
 	}
 }
---
+++ Assets/Scripts/PickupItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickupItem : ItemScript{
+
+	public static event ItemAction OnItemPickup;
+
+	public void OnTriggerEnter(Collider col){
+		GetComponentInChildren<MeshRenderer> ().enabled = false;
+		OnItemPickup (this);
+	}
+}
+
