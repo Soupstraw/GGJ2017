d73134075160b4359bacfc678360d269654ccda8
Sarge <sercan.altundas@gmail.com>
2017-01-21 15:55:06 +0200

    Slide helper script added and tested

---
+++ Assets/Scripts/SlideHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideHelper : MonoBehaviour {
+
+	private Vector3 startPoint;
+	private Vector3 endPoint;
+	private Vector3 distance = new Vector3(0, -5, 0);
+	private bool clicked = false;
+
+	public float speed;
+	public float max;
+
+	void Start(){
+		startPoint 	= gameObject.transform.position;
+		endPoint	= startPoint + distance;
+	}
+
+	void OnMouseDown() {
+        Debug.Log("test");
+		clicked = true;
+    }
+
+	void Update(){
+		if(clicked && transform.position.z > -max)
+    		transform.position += distance * Time.deltaTime * 10;
+	}
+}
