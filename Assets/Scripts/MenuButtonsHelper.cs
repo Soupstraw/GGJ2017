4bdfb85a4f6ab16cd97b53584fefd5281fd4d6f9
Sarge <sercan.altundas@gmail.com>
2017-01-21 18:24:52 +0200

    menu buttons created

---
+++ Assets/Scripts/MenuButtonsHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonsHelper : MonoBehaviour {
+
+	void OnMouseDown() {
+		if(gameObject.name == "Play"){
+			Application.LoadLevel("intro");
+		}
+		else if(gameObject.name == "Credits"){
+			Application.LoadLevel("credits");
+		}
+		else if(gameObject.name == "Quit"){
+			Application.Quit();
+		}
+    }
+}
