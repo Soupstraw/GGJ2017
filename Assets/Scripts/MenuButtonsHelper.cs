b42f7f1f58e7ed65372a8fd07af60c2beea4f0a7
Sarge <sercan.altundas@gmail.com>
2017-01-22 01:11:01 +0200

    menu button mouse over color change

--- Assets/Scripts/MenuButtonsHelper.cs
+++ Assets/Scripts/MenuButtonsHelper.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuButtonsHelper : MonoBehaviour {
+	private Renderer rend;
+
+	void Start() {
+        Shader rend = GetComponent<Shader>();
+    }
 
 	void OnMouseDown() {
 		if(gameObject.name == "Play"){
+			transform.localScale += new Vector3(0.1f, 0.1f, 0);
 			SceneManager.LoadScene ("Intro");
 		}
 		else if(gameObject.name == "Quit"){
 			Application.Quit();
 		}
     }
+
+	void OnMouseOver(){
+        gameObject.GetComponent<Renderer> ().material.color = Color.red;
+	}
+
+	void OnMouseExit(){
+        gameObject.GetComponent<Renderer> ().material.color = Color.blue;
+	}
 }
