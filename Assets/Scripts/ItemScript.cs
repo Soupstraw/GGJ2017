2a761579f166909a2a9c2f0f314ab3e771621c37
Joosep Jääger <joosep.jaager@gmail.com>
2017-01-21 02:12:40 +0200

    Added pen pickup

---
+++ Assets/Scripts/ItemScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col){
+		OnItemPickup ();
+		GetComponentInChildren<MeshRenderer> ().enabled = false;
+		GetComponent<Collider> ().enabled = false;
+	}
+
+	public virtual void OnItemPickup(){
+		
+	}
+}
---
+++ Assets/Scripts/PenItemScript.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class PenItemScript : ItemScript{
+
+	public SkinnedMeshRenderer penMeshRenderer;
+
+	public override void OnItemPickup(){
+		penMeshRenderer.enabled = true;
+	}
+}
+
